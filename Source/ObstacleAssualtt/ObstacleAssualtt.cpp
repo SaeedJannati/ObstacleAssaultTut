40c532381aa9c7f328cfb3c82187c71d4aab0572
Saeed Jannati <msaeedjannati72@gmail.com>
2023-10-17 01:46:28 +0330

    Adding character and a new class

---
+++ Source/ObstacleAssualtt/ObstacleAssualtt.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ObstacleAssualtt.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ObstacleAssualtt, "ObstacleAssualtt" );
